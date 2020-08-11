8443953084b0aa88b57c2eee09721376f64bd1f5
Your Name <ryibrahem1210@gmail.com>
2020-08-10 21:12:50 -0400

    initial comit

---
+++ build/precache-manifest.d4518aa05a72e1e16b03a67d8c7ccf57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00c5a20e5ce2c6d0e4bf818240e8b674",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77aff1a2d99cf170ec8f",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "4969b27afad658c369bd",
+    "url": "/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "77aff1a2d99cf170ec8f",
+    "url": "/static/js/2.50967932.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.50967932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4969b27afad658c369bd",
+    "url": "/static/js/main.f7f1a816.chunk.js"
+  },
+  {
+    "revision": "81ed1e4f7707c590606d",
+    "url": "/static/js/runtime-main.3a0c7701.js"
+  }
+]);
